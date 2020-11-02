42e7c7d75e6ce0e07d3aff770c8137710636dc87
Holzner Anton <anton.holzner@fliegl.com>
2020-11-02 07:32:34 +0100

    better debugging with this helper Function.

--- components/esp_rom/include/esp32/rom/ets_sys.h
+++ components/esp_rom/include/esp32/rom/ets_sys.h
@@ -633,6 +633,11 @@ void intr_matrix_set(int cpu_no, uint32_t model_num, uint32_t intr_num);
 #define MACSTR "%02x:%02x:%02x:%02x:%02x:%02x"
 #endif
 
+#ifndef PAYLOAD2STR
+#define PAYLOAD2STR(a) (a)[0], (a)[1], (a)[2], (a)[3], (a)[4], (a)[5], (a)[6], (a)[7], (a)[8], (a)[9], (a)[10], (a)[11], (a)[12], (a)[13], (a)[14], (a)[15]
+#define PAYLOADSTR "%02x:%02x:%02x:%02x:%02x:%02x:%02x:%02x:%02x:%02x:%02x:%02x:%02x:%02x:%02x:%02x"
+#endif
+
 #define ETS_MEM_BAR() asm volatile ( "" : : : "memory" )
 
 typedef enum {
